d1ab395d30f2b1123e8a4c349c7bc4497fbdf4ef
enenchepeter <enenche.peter@techvibesltd.com>
2021-09-14 19:54:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ulessontest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ulessontest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_live_home)
+    }
+}
